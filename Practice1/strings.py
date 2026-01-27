868aaeef9a5ad1086bb8a23a339ac572e1716eb5
Toleukali Elnara <etoleukali07@gmail.com>
2026-01-27 22:27:40 +0500

    practice1 in progress

---
+++ Practice1/strings.py
@@ -0,0 +1,27 @@
+#1
+a = "My name is Elnara!"
+print(a[-12:-5])
+
+#2
+a = "Hello, World!"
+print(a.split(","))
+
+#3
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#4
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#5
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+#6
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
