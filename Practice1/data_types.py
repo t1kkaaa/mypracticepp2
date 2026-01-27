8f72f9b2081c199b98de22596b7b756b317add54
Toleukali Elnara <etoleukali07@gmail.com>
2026-01-27 22:58:54 +0500

    Setting the Specific Data Type

---
+++ Practice1/data_types.py
@@ -0,0 +1,29 @@
+#1
+x = complex(1j)
+print(x)
+print(type(x)) 
+
+#2
+x = tuple(("apple", "banana", "cherry"))
+print(x)
+print(type(x))
+
+#3
+x = dict(name="John", age=36)
+print(x)
+print(type(x))
+
+#4
+x = frozenset(("apple", "banana", "cherry"))
+print(x)
+print(type(x)) 
+
+#5
+x = bytes(5)
+print(x)
+print(type(x)) 
+
+#6
+x = memoryview(bytes(5))
+print(x)
+print(type(x)) 
