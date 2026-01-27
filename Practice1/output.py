d5323f9e8a3d961769703ae9cf8f12478674106e
Toleukali Elnara <etoleukali07@gmail.com>
2026-01-27 23:03:33 +0500

    work with print

---
+++ Practice1/output.py
@@ -0,0 +1,19 @@
+#1
+print("This will work!")
+print('This will also work!')
+
+#2
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+#3
+print(3 + 3)
+print(2 * 5)
+
+#4
+print("I am", 35, "years old.")
+
+#5
+print(3)
+print(358)
+print(50000)
