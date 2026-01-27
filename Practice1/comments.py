595abb9e6169f3ce8d224f4c72169f573139c9be
Toleukali Elnara <etoleukali07@gmail.com>
2026-01-27 23:01:48 +0500

    how write comments

---
+++ Practice1/comments.py
@@ -0,0 +1,24 @@
+#1
+#This is a comment
+print("Hello, World!")
+
+#2
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#3
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+#4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#5
+print("Hello, World!") #This is a comment
