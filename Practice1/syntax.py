860c760d4a6c9799ea80e1e32f684a13c916fbf4
Toleukali Elnara <etoleukali07@gmail.com>
2026-01-27 23:08:36 +0500

    syntax and statemens

---
+++ Practice1/syntax.py
@@ -0,0 +1,27 @@
+#1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#3
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+#4
+#This is a comment.
+print("Hello, World!")
+
+#5
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+#6
+print("Hello"); print("How are you?"); print("Bye bye!")
